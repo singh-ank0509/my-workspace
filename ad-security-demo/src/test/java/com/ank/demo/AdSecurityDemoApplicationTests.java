b30379d438809691ffeba3f8fc9921dadc5fc523
singh-ank0509 <117641692+singh-ank0509@users.noreply.github.com>
2025-12-12 20:59:43 +0530

    Add files via upload

---
+++ ad-security-demo/src/test/java/com/ank/demo/AdSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ank.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
